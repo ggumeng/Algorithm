35f8e76b418e4cbba16784419503dee6ab67a662
ghgwon <54825946+ggumeng@users.noreply.github.com>
2025-02-19 20:49:01 +0900

    [Bronze II] Title: 16진수, Time: 120 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //솔루션 코드 작성
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(Integer.parseInt(bf.readLine(), 16) + "\n");
+        
+        bw.flush();
+        bw.close();
+    }
+}
