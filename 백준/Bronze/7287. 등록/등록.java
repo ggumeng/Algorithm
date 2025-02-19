152682178b0939b01afa3dea0ccb2eddf7fd8e8b
ghgwon <54825946+ggumeng@users.noreply.github.com>
2025-02-19 20:23:49 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //솔루션 코드 작성
+        // BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(63 + "\n");
+        bw.write("kkh47406");
+        
+        bw.flush();
+        bw.close();
+    }
+}
