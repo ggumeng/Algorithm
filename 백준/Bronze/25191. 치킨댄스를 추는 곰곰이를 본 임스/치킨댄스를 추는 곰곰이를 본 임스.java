5c80b666a8a077187e285437d950ee6a463c21fc
ghgwon <54825946+ggumeng@users.noreply.github.com>
2025-03-13 21:48:25 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 108 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int chicken = Integer.parseInt(bf.readLine());
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int coke = Integer.parseInt(st.nextToken());
+        int beer = Integer.parseInt(st.nextToken());
+
+        if (coke / 2 + beer <= chicken) {
+            System.out.println(coke / 2 + beer);
+        } else {
+            System.out.println(chicken);
+        }
+    }   
+}
