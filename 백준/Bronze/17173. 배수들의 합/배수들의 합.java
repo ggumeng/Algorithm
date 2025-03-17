91612d0869c8d1faa0065d0c4c4a47ebf2769e72
ghgwon <54825946+ggumeng@users.noreply.github.com>
2025-03-17 23:05:14 +0900

    [Bronze II] Title: 배수들의 합, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/17173. 배수들의 합/배수들의 합.java
+++ 백준/Bronze/17173. 배수들의 합/배수들의 합.java
@@ -4,40 +4,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        List<Integer> arr = new ArrayList<>();
-
+        boolean[] isMultiple = new boolean[n + 1]; // 비트마스크 대신 배열 사용
         st = new StringTokenizer(bf.readLine());
 
         while (m-- > 0) {
             int k = Integer.parseInt(st.nextToken());
-
-            int temp = k;
-            int num = 1;
-
-            while (true) {
-                temp = k * num;
-
-                if (temp > n) break;
-
-                arr.add(temp);
-                num++;
+            for (int i = k; i <= n; i += k) {
+                isMultiple[i] = true;
             }
         }
 
-        Set<Integer> setArr = new HashSet<>(arr);
-
         int sum = 0;
-
-        for (Integer integer : setArr) {
-            sum += integer;
+        for (int i = 1; i <= n; i++) {
+            if (isMultiple[i]) sum += i;
         }
 
-        System.out.println(sum);
-    }   
+        System.out.println(sum); // System.out.println()이 오히려 더 빠를 수 있음
+    }
 }
